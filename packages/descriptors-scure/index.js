63b4fa9cb1dce64ce96cc1360190868dc1bf7101
José Luis Landabaso Díaz <landabaso@gmail.com>
2026-03-25 15:43:53 +0100

    fix(packages): expose wrapper exports explicitly for ESM consumers

--- packages/descriptors-scure/index.js
+++ packages/descriptors-scure/index.js
@@ -8,10 +8,21 @@ const { secp256k1 } = require('@noble/curves/secp256k1.js');
 
 const bound = core.DescriptorsFactory(createScureLib());
 
-module.exports = {
-  ...core,
-  ...bound,
-  btc,
-  HDKey,
-  secp256k1
-};
+exports.networks = core.networks;
+exports.DescriptorsFactory = core.DescriptorsFactory;
+exports.checksum = core.checksum;
+exports.signers = core.signers;
+exports.keyExpressionBIP32 = core.keyExpressionBIP32;
+exports.keyExpressionLedger = core.keyExpressionLedger;
+exports.scriptExpressions = core.scriptExpressions;
+exports.ledger = core.ledger;
+
+exports.Output = bound.Output;
+exports.parseKeyExpression = bound.parseKeyExpression;
+exports.expand = bound.expand;
+exports.ECPair = bound.ECPair;
+exports.BIP32 = bound.BIP32;
+
+exports.btc = btc;
+exports.HDKey = HDKey;
+exports.secp256k1 = secp256k1;
